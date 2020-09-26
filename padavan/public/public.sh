675560716253b561dc0140de13c914f3e2019a95
chengxudong2020 <872603935@qq.com>
2020-09-26 12:17:59 +0800

    Update public.sh

--- padavan/public/public.sh
+++ padavan/public/public.sh
@@ -7,6 +7,16 @@ DESTDIR=/opt/rt-n56u
  echo "--------------开始复制默认配置文件----------"
 \cp -f ../public/defaults.h ${DESTDIR}/trunk/user/shared/defaults.h
  echo "--------------开始复制默认配置文件结束----------"
+ 
+default_theme_path="${DESTDIR}/trunk/user/www/n56u_ribbon_fixed"
+
+    echo "--------------开始复制主题----------------------"
+	sudo rm -rf $default_theme_path/bootstrap
+	sudo rm -rf $default_theme_path/images
+	cp -rf ../public/theme/. $default_theme_path
+	echo "--------------复制主题文件结束------------------"
+ 
+ 
 
    echo "--------------开始复制中文SSID支持文件----------"
  # trunk/user/httpd/aspbw.c
@@ -18,4 +28,7 @@ DESTDIR=/opt/rt-n56u
 \cp -f ../public/SSID/cmm_info.c ${DESTDIR}/trunk/proprietary/rt_wifi/rtsoc/2.7.X.X/rt2860v2/common/cmm_info.c
 \cp -f ../public/SSID/ralink.c ${DESTDIR}/trunk/user/httpd/ralink.c
     echo "--------------复制中文SSID支持文件结束----------"
+	
+	
+	
  
