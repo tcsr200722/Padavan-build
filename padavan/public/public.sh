1d8274a0880fd8bd94d4f558d2f37e9ee884a2fa
chengxudong2020 <872603935@qq.com>
2020-09-26 12:36:08 +0800

    fix ssid

--- padavan/public/public.sh
+++ padavan/public/public.sh
@@ -22,11 +22,11 @@ default_theme_path="${DESTDIR}/trunk/user/www/n56u_ribbon_fixed"
  # trunk/user/httpd/aspbw.c
  # trunk/user/httpd/httpd.h
  # trunk/user/www/n56u_ribbon_fixed/general.js
-\cp -f ../public/SSID/aspbw.c ${DESTDIR}/trunk/user/httpd/aspbw.c
-\cp -f ../public/SSID/httpd.h ${DESTDIR}/trunk/user/httpd/httpd.h
-\cp -f ../public/SSID/general.js ${DESTDIR}/trunk/user/www/n56u_ribbon_fixed/general.js
+# \cp -f ../public/SSID/aspbw.c ${DESTDIR}/trunk/user/httpd/aspbw.c
+# \cp -f ../public/SSID/httpd.h ${DESTDIR}/trunk/user/httpd/httpd.h
+# \cp -f ../public/SSID/general.js ${DESTDIR}/trunk/user/www/n56u_ribbon_fixed/general.js
 \cp -f ../public/SSID/cmm_info.c ${DESTDIR}/trunk/proprietary/rt_wifi/rtsoc/2.7.X.X/rt2860v2/common/cmm_info.c
-\cp -f ../public/SSID/ralink.c ${DESTDIR}/trunk/user/httpd/ralink.c
+# \cp -f ../public/SSID/ralink.c ${DESTDIR}/trunk/user/httpd/ralink.c
     echo "--------------复制中文SSID支持文件结束----------"
 	
 	
