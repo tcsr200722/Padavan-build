3b6e20eeddbcae0c77d390f385b87832b242fe01
chengxudong2020 <872603935@qq.com>
2020-09-26 11:21:58 +0800

    fix

--- padavan/K2P/K2P.sh
+++ padavan/K2P/K2P.sh
@@ -1,4 +1,3 @@
 ROOTDIR=`pwd`
 DESTDIR=/opt/rt-n56u
-echo "执行如下命令sed -i \"s/BOARD_PID/\"PHICOMM\"/g\" $default_path"
 sed -i "s/BOARD_PID/\"PHICOMM\"/g" ${DESTDIR}/trunk/user/shared/defaults.h
--- padavan/PSG1218/PSG1218.sh
+++ padavan/PSG1218/PSG1218.sh
@@ -1,9 +1,4 @@
 # 是否超频 
 # echo "CONFIG_FIRMWARE_CPU_600MHZ=y" >> .config
-default_path='./user/shared/defaults.h'  # 默认文件配置目录
-echo "执行如下命令sed -i \"s/BOARD_PID/\"PHICOMM\"/g\" $default_path"
-sed -i "s/BOARD_PID/\"PHICOMM\"/g" $default_path
-default_theme_path='./user/www/n56u_ribbon_fixed'
-sudo rm -rf $default_theme_path/bootstrap
-sudo rm -rf $default_theme_path/images
-cp -rf ../theme/. $default_theme_path
+DESTDIR=/opt/rt-n56u
+sed -i "s/BOARD_PID/\"PHICOMM\"/g" ${DESTDIR}/trunk/user/shared/defaults.h
