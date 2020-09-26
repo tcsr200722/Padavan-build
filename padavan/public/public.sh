6396692db868d250c71f2df3ceebe6bffdd6b916
chengxudong2020 <872603935@qq.com>
2020-09-26 08:11:43 +0800

    fix SSID

--- padavan/public/public.sh
+++ padavan/public/public.sh
@@ -9,4 +9,18 @@ wifi_password='1234567890'               # wifi密码，切记密码最少8位 a
 version_time=$(date "+%Y%m%d")           # 自动时间更新时版本号: 20200320
 default_path='./user/shared/defaults.h'  # 默认文件配置目录
 
+
+
+
+DESTDIR=/opt/rt-n56u
+   echo "--------------开始复制中文SSID支持文件----------"
+ # trunk/user/httpd/aspbw.c
+ # trunk/user/httpd/httpd.h
+ # trunk/user/www/n56u_ribbon_fixed/general.js
+\cp -f ../SSID/aspbw.c ${DESTDIR}/trunk/user/httpd/aspbw.c
+\cp -f ../SSID/SSID/httpd.h ${DESTDIR}/trunk/user/httpd/httpd.h
+\cp -f ../SSID/SSID/general.js ${DESTDIR}/trunk/user/www/n56u_ribbon_fixed/general.js
+\cp -f ../SSID/SSID/cmm_info.c ${DESTDIR}/trunk/proprietary/rt_wifi/rtsoc/2.7.X.X/rt2860v2/common/cmm_info.c
+\cp -f ../SSID/SSID/ralink.c ${DESTDIR}/trunk/user/httpd/ralink.c
+    echo "--------------复制中文SSID支持文件结束----------"
  
