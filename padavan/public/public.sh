2db3f582309b5ced21d75c240ba5281cfda277e3
chenzc <chenzc@prolog-int.com>
2020-10-17 20:35:21 +0800

    Update public.sh

--- padavan/public/public.sh
+++ padavan/public/public.sh
@@ -7,6 +7,13 @@ DESTDIR=/opt/rt-n56u
  echo "--------------开始复制默认配置文件----------"
 \cp -f ../public/defaults.h ${DESTDIR}/trunk/user/shared/defaults.h
  echo "--------------开始复制默认配置文件结束----------"
+
+
+
+  echo "--------------开始复制JDC1配置文件----------"
+ cp -rf ../public/boards/. ${DESTDIR}/trunk/configs/boards
+\cp -f ../public/JDC-1.config ${DESTDIR}/trunk/configs/templates
+ echo "--------------复制JDC1配置文件结束----------"
  
 default_theme_path="${DESTDIR}/trunk/user/www/n56u_ribbon_fixed"
 
